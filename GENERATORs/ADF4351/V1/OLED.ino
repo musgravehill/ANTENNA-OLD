99b5206fb03f92eed9e66f9260ca8bcc58fa52a5
Bob_work <musgravehill@gmail.com>
2018-02-27 22:42:06 +0300

    oled

--- GENERATORs/ADF4351/V1/OLED.ino
+++ GENERATORs/ADF4351/V1/OLED.ino
@@ -20,12 +20,14 @@ void OLED_display() {
   if (ADF4351_isNeedSetNewConfig) {
     if (OLED_blynk_state) {
       myOLED.print("SET", 110, 41);
-      digitalWrite(LED_pin,1);
+      //digitalWrite(LED_pin, 1);
       OLED_blynk_state = false;
     } else {
-      digitalWrite(LED_pin,0);
+      //digitalWrite(LED_pin, 0);
       OLED_blynk_state = true;
     }
+  } else {
+    //digitalWrite(LED_pin, 0);
   }
 
   myOLED.setFont(MediumNumbers);
@@ -37,7 +39,7 @@ void OLED_display() {
   uint16_t f_m = (INTERFACE_freq_multiplier_variants[INTERFACE_freq_multiplier_current] * ADF4351_frequency) / 100000L; //*10Hz
   uint32_t f_k =  ((INTERFACE_freq_multiplier_variants[INTERFACE_freq_multiplier_current] * ADF4351_frequency) % 100000); //*10Hz
   myOLED.printNumI(f_m, 0, 0, 5, '0');
-  myOLED.printNumI(f_k, 68, 0, 5, '0');  
+  myOLED.printNumI(f_k, 68, 0, 5, '0');
 
   myOLED.print(OLED_stepsVariants_val[ADF4351_stepsVariantsNumCurrent], 26, 18);
 
